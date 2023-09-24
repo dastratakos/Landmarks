cb30eb0e7b6d365761d08ec7475c4e2b8c7b7bf6
Dean Stratakos <dastratakos@gmail.com>
2023-09-24 15:40:07 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Dean Stratakos on 9/24/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
